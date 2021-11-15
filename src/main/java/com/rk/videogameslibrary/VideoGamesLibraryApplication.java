d1b94b4e3cdabdd64304d61cd2d367f6bda5dab9
RaphaelKimm <raphael.kimm8@etu.univ-lorraine.fr>
2021-11-15 12:27:18 +0100

    Commit initial - création d'une API REST de base

---
+++ src/main/java/com/rk/videogameslibrary/VideoGamesLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.rk.videogameslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGamesLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGamesLibraryApplication.class, args);
+	}
+
+}
